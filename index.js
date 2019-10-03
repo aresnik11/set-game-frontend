4b2bd668f618ed63648187dd794f3fde4289d1fd
aresnik11 <aresnik11@gmail.com>
2019-10-03 15:33:33 -0400

    added high score functionality and some other css fixes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const menu = document.querySelector('.menu')
 const options = document.querySelector('.options')
 const timerContainer = options.querySelector('#timer')
 const cardNum = options.querySelector('#cardnum')
+const login = document.querySelector('.login')
+const loginForm = login.querySelector('form')
 const board = document.querySelector('.board')
 const rulesModal = document.querySelector('.my-modal.rules')
 const scoresModal = document.querySelector('.my-modal.scores')
@@ -10,33 +12,44 @@ const gameOverModal = document.querySelector('.my-modal.game-over')
 let shuffledCards = []
 let selectedCounter = 0
 let timer;
+let currentUser;
+
+loginForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const name = e.target.name.value
+    fetch(`http://localhost:3000/api/v1/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+        })
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(user) {
+        currentUser = user
+        login.innerHTML =
+            `<h1>Welcome, ${currentUser.name}</h1>
+            <h3>To play, click New Game</h3>`
+    })
+})
 
 //create new game on click of the new game button
 document.addEventListener('click', function(e) {
-    if (e.target.dataset.action === "create") {
-        fetch("http://localhost:3000/api/v1/games", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-                score: 0,
-                status: "in progress"
-            })
-        })
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(game) {
-            //reset board and options, if there already was a board
-            board.innerHTML = ""
-            timerContainer.innerHTML = ""
-            cardNum.innerHTML = ""
-            selectedCounter = 0
-            //initialize the game
-            initializeGame(game)
-        })
+    //only create a new game on click of new game when user exists
+    if (e.target.dataset.action === "create" && currentUser) {
+        createGame()
+    }
+    //if someone clicks new game and user does not exist, prompt them to log in
+    else if (e.target.dataset.action === "create" && !currentUser) {
+        if (!login.querySelector('h3')) {
+            login.insertAdjacentHTML("afterbegin",
+            `<h3>Please enter your name to play</h3>`)
+        }
     }
     //shows rules modal if you click on the rules menu option
     else if (e.target.dataset.action === "rules") {
@@ -44,36 +57,49 @@ document.addEventListener('click', function(e) {
     }
     //shows scores modal if you click on the scores menu option
     else if (e.target.dataset.action === "scores") {
-        fetch("http://localhost:3000/api/v1/games")
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(games) {
-            scoresModal.style.display = "block"
-            for (let i=0; i<10; i++) {
-                scoresModal.querySelector('ol').insertAdjacentHTML("beforeend",
-                `<li>${games[i].score}</li>`)
-            }
-        })
+        fetchGames()
     }
 })
+
+//creates a new game in the DB and starts setting it up on the frontend
+function createGame() {
+    fetch("http://localhost:3000/api/v1/games", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            score: 0,
+            status: "in progress",
+            user_id: parseInt(currentUser.id)
+        })
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(game) {
+        login.innerHTML = ""
+        resetBoard()
+        initializeGame(game)
+    })
+}
 
 //closes any of the modals if you click anywhere on the screen (modal becomes the whole screen)
 document.addEventListener('click', function(e) {
     if (e.target.classList.contains('my-modal')) {
         e.target.style.display = "none"
     }
-    // rulesModal.style.display = "none"
 })
 
-// scoresModal.addEventListener('click', function(e) {
-//     scoresModal.style.display = "none"
-// })
-
-// gameOverModal.addEventListener('click', function(e) {
-//     gameOverModal.style.display = "none"
-// })
-
+//resets the board, timer, and all options
+function resetBoard() {
+    board.innerHTML = ""
+    timerContainer.innerHTML = ""
+    clearInterval(timer)
+    cardNum.innerHTML = ""
+    selectedCounter = 0
+}
 
 //sets up initial board and game, fetches cards from DB
 function initializeGame(game) {
@@ -82,7 +108,9 @@ function initializeGame(game) {
         return response.json()
     })
     .then(function(cards) {
+        board.dataset.id = game.id
         shuffleCards(cards)
+        renderTimer()
         renderOptions()
         renderInitialCards(shuffledCards)
         removeCards(12)
@@ -90,7 +118,7 @@ function initializeGame(game) {
     })
 }
 
-//randomly shuffles the cards
+//randomly shuffles the cards and stores them in the shuffled cards array
 function shuffleCards(cards) {
     shuffledCards = []
     while (cards.length) {
@@ -107,26 +135,6 @@ function renderInitialCards(cards) {
     }
 }
 
-function svgBuilder(card) {
-    let svg = ""
-    let num
-
-        if (card.number === "one") {
-            num = 1
-        }
-        else if (card.number === "two") {
-            num = 2
-        }
-        else if (card.number === "three") {
-            num = 3
-        }
-
-    for (let i = 0; i < num; i++) {
-        svg += `<img src="./assets/${card.color}-${card.fill}-${card.shape}.svg">`
-    }
-    return svg
-}
-
 //adds a single card to the window with all of the card attributes
 function renderSingleCard(card) {
     let svg = svgBuilder(card)
@@ -136,8 +144,28 @@ function renderSingleCard(card) {
     </div>`)
 }
 
-//adds no set button and card count after board has been created
-function renderOptions() {
+//builds card images
+function svgBuilder(card) {
+    let svg = ""
+    let num
+    if (card.number === "one") {
+        num = 1
+    }
+    else if (card.number === "two") {
+        num = 2
+    }
+    else if (card.number === "three") {
+        num = 3
+    }
+
+    for (let i = 0; i < num; i++) {
+        svg += `<img src="./assets/${card.color}-${card.fill}-${card.shape}.svg">`
+    }
+    return svg
+}
+
+//adds timer and pause button after board has been created
+function renderTimer() {
     timerContainer.insertAdjacentHTML("beforeend",
     `<div class="ui statistic">
         <div class="value">
@@ -151,6 +179,10 @@ function renderOptions() {
         <i class="pause icon"></i>
         Pause
     </button>`)
+}
+
+//adds no set button and card count after board has been created
+function renderOptions() {
     cardNum.insertAdjacentHTML("beforeend",
     `<div class="ui statistic">
         <div class="value">
@@ -171,18 +203,44 @@ function removeCards(numCards) {
     cardNum.querySelector(".value").innerText = shuffledCards.length
 }
 
+//starts the clock and increments the timer every second
 function startTimer() {
     timer = setInterval(function() {
         let current = timerContainer.querySelector(".value")
-        // debugger;
         current.innerText = parseInt(current.innerText) + 1
-    }, 1000)
+    },1000)
+}
+
+//fetch games from DB - only sending top 10 scores
+function fetchGames() {
+    fetch("http://localhost:3000/api/v1/games")
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(games) {
+        renderHighScores(games)
+    })
+}
+
+//shows the scores modal, resets scores, renders score for each game we got back
+function renderHighScores(games) {
+    scoresModal.style.display = "block"
+    scoresModal.querySelector('ol').innerHTML = ""
+    games.forEach(function(game) {
+        renderHighScore(game)
+    })
+}
+
+//renders game score and adds to the scores ul as an li
+//have access to game.user.name since passing in user along with game in the backend
+function renderHighScore(game) {
+        scoresModal.querySelector('ol').insertAdjacentHTML("beforeend",
+        `<li>${game.user.name} - ${game.score} seconds</li>`)
 }
 
 //listens for clicks on the board
 board.addEventListener("click", function(e) {
     if (e.target.closest('.card')) {
-
         //if it already was selected, remove the border so it is no longer selected
         if (e.target.closest('.card').classList.contains("selected")) {
             removeSelected(e.target.closest('.card'))
@@ -195,24 +253,32 @@ board.addEventListener("click", function(e) {
                 const selectedCards = board.querySelectorAll('.card.selected')
                 //if it is a set, swap out the cards and show new ones
                 if (checkForSet(selectedCards)) {
-                    selectedCards.forEach(function(card) {
-                        card.classList.add("fadeout")
-                    })
-                    setTimeout(function() {swapCards(selectedCards)
-                    }, 500)
+                    //slow down adding 'fadeout' class so third card spins before the cards fade
+                    setTimeout(function() {
+                        selectedCards.forEach(function(card) {
+                            card.classList.add("fadeout")
+                        })
+                    },700)
+                    //slow down swapping out cards
+                    setTimeout(function() {
+                        swapCards(selectedCards)
+                    },1200)
                 }
                 //if it isn't a set, unselect those cards
                 else {
-                    selectedCards.forEach(function(card) {
-                        card.classList.add("wrong")
-                    })
-                    //slow down removing selected, so the third card spins if we don't have a set
+                    //slow down adding "wrong" class so third card spins before they bounce in red
+                    setTimeout(function() {
+                        selectedCards.forEach(function(card) {
+                            card.classList.add("wrong")
+                        })
+                    },700)
+                    //slow down removing selected, so the third card spins back if we don't have a set
                     setTimeout(function() {
                         selectedCards.forEach(function(card) {
                             card.classList.remove("wrong")
                             removeSelected(card)
                         })
-                    },900)
+                    },1200)
                 }
             }
         }
@@ -237,6 +303,7 @@ function removeSelected(element) {
 function swapCards(selectedCards) {
     selectedCards.forEach(function(card) {
         removeSelected(card)
+        //if there are still cards to be dealt in the deck, deal a new card and swap it with the one from the set
         if (shuffledCards.length) {
             newCard = shuffledCards[0]
             removeCards(1)
@@ -244,18 +311,18 @@ function swapCards(selectedCards) {
 
            setTimeout(function() {
                 card.classList.remove("fadein")
-           },500)
+           },1200)
         }
+        //if there aren't any more cards in the deck, just remove the cards that made a set
         else {
             card.remove()
             const boardCards = board.querySelectorAll('.card')
             setTimeout(function() {
+                //if there aren't any more cards on the board, game is over.
                 if (!boardCards.length) {
-                    gameOverModal.querySelector('p').innerText = "No cards left!"
-                    gameOverModal.style.display = "block"
+                    gameOver("No cards left!")
                 }
-            }, 500)
-
+            },500)
         }
     })
 }
@@ -271,19 +338,28 @@ function swapCard(card, newCard) {
     card.dataset.fill = `${newCard.fill}`
 }
 
-// function getThreeCardsAttributes(selectedCards) {
-//     const numberArr = []
-//     const colorArr = []
-//     const shapeArr = []
-//     const fillArr = []
+//show modal prompting user to start a new game, stops timer and removes pause/play button
+//update game status and score in the DB
+function gameOver(text) {
+    gameOverModal.querySelector('p').innerText = text
+    gameOverModal.style.display = "block"
+    clearInterval(timer)
+    timerContainer.querySelector('button').remove()
 
-//     selectedCards.forEach(function(card) {
-//         numberArr.push(card.dataset.number)
-//         colorArr.push(card.dataset.color)
-//         shapeArr.push(card.dataset.shape)
-//         fillArr.push(card.dataset.fill)
-//     })
-// }
+    const score = parseInt(timerContainer.querySelector(".value").innerText)
+
+    fetch(`http://localhost:3000/api/v1/games/${board.dataset.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            score: score,
+            status: "completed"
+        })
+    })
+}
 
 //gets attributes from the 3 selected cards and groups them
 //if they are all the same or all different for every attribute, return true (it is a set)
@@ -371,16 +447,16 @@ options.addEventListener("click", function(e) {
             cardsOnBoard.forEach(function(card) {
                 card.classList.add("wrong")
             })
-            //slow down removing selected, so the third card spins if we don't have a set
+            //slow down removing 'wrong', so the cards bounce
             setTimeout(function() {
                 cardsOnBoard.forEach(function(card) {
                     card.classList.remove("wrong")
                 })
             },500)
-            console.log("there is a set on the board")
         }
-        //if there is not a set on the board, reshuffle the cards
+        //if there is not a set on the board
         else {
+            //if there are still cards in the deck, reshuffle the cards including the ones on the board and rerender the board
             if (shuffledCards.length) {
                     cardsOnBoard.forEach(function(card) {
                         card.classList.add("fadeout")
@@ -388,14 +464,14 @@ options.addEventListener("click", function(e) {
                 setTimeout(function() {
                     reshuffleAndRerender(cardsOnBoard)
                 }, 1000)
-
             }
+            //if there aren't any cards left in the deck, game is over
             else {
-                gameOverModal.querySelector('p').innerText = "There are no more sets and no more cards in the deck"
-                gameOverModal.style.display = "block"
+                gameOver("There are no more sets and no more cards in the deck")
             }
         }
     }
+    //if pause button was clicked, stop the timer, change button to play button, fade the board out
     else if (e.target.dataset.action === "pause") {
         clearInterval(timer)
         e.target.dataset.action = "play"
@@ -403,6 +479,7 @@ options.addEventListener("click", function(e) {
         board.classList.remove("fadein")
         board.classList.add("fadeout")
     }
+    //if play button was clicked, start the timer, change button to pause button, fade in the board
     else if (e.target.dataset.action === "play") {
         startTimer()
         e.target.dataset.action = "pause"
@@ -410,13 +487,19 @@ options.addEventListener("click", function(e) {
         board.classList.remove("fadeout")
         board.classList.add("fadein")
     }
+    //if any letters of the logo are clicked, add class to make them spin
+    else if (e.target.parentElement.id === "logo") {
+        e.target.classList.add("spin")
+        setTimeout(function() {
+            e.target.classList.remove("spin")
+        },900)
+    }
 })
 
 //calls fetchCard on an array of cards (ones that are currently on the board)
 //uses Promise.all to collect all of the promises and then when they are all done
 //add cards on board to shuffledCards array, reshuffle, and repopulate board with cards
 function reshuffleAndRerender(cards) {
-
     const promises = []
     cards.forEach(function(card) {
         promises.push(fetchCard(card))
